6eec190f4b1235c6130d45562db961a0e667dd1f
james <github@zx85.me>
2026-05-30 19:52:12 +0100

    add a bit for the public channel when the time comes

--- flask-app/app.py
+++ flask-app/app.py
@@ -100,6 +100,7 @@ def start_background_monitor():
         "status_topic": os.environ.get("MQTT_STATUS_TOPIC", "meshcore/status"),
         "message_topic": os.environ.get("MQTT_MESSAGE_TOPIC", "meshcore/messages"),
         "advert_topic": os.environ.get("MQTT_ADVERT_TOPIC", "meshcore/advert"),        
+        "advert_public": os.environ.get("MQTT_PUBLIC_TOPIC", "meshcore/public"),        
     }
 
     try:
